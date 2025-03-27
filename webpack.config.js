d6e77dee7ec78335e9ff101b7b9ffcf189dfe5e1
FadhliRajwaa <fadhlirajwaarahmana@gmail.com>
2025-03-27 13:49:20 +0700

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html'
-        })
-    ],
-    devServer: {
-        static: path.resolve(__dirname, 'dist'),
-        port: 8080,
-        open: true
-    },
-    mode: 'development'
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  mode: 'production'
 };
